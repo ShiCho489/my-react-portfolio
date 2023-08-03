cebcde00ce8708fbeff88fbb7cdeb82504676295
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-08-04 00:35:58 +0300

    updated

--- shivaniwebsite/src/App.js
+++ shivaniwebsite/src/App.js
@@ -2,6 +2,10 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
+import github from "./assets/github.png"
+import linkedin from "./assets/lnk.png";
+import twitter from "./assets/twitter.png";
+import Social from "./components/Social/Social";
 
 
 function App() {
@@ -15,6 +19,19 @@ function App() {
       </div>
       <div className="page" id="about">
         <About />
+      </div>
+      <div className="socials-wrapper">
+        <h2 className="quick-links">Links</h2>
+        <div className="socials">
+          <Social logo={github} title="GitHub" />
+          <Social logo={linkedin} title="LinkedIn" />
+          <Social logo={twitter} title="Twitter" />
+          
+
+        </div>
+        <div className="page" id="projects">
+         <Project />
+        </div>
       </div>
     </main>
 
