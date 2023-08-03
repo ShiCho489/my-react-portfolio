851c7789d4867c190a4f33277983c0c69632cc4d
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-08-03 21:16:35 +0300

    root component updated

--- shivaniwebsite/src/App.js
+++ shivaniwebsite/src/App.js
@@ -1,15 +1,22 @@
 import "./App.scss";
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
 
 
 function App() {
   return (
     <div className="App">
-     
-
      <Header />
       
-  
+    <main>
+      <div className="page" id="home">
+        <Home />
+      </div>
+      <div className="page" id="about">
+        <About />
+      </div>
+    </main>
 
  
      
