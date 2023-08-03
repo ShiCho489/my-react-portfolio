36b9deffc605639432258c6012edb2dacdd1f952
Shivani Choudhary <choudhary.shivani489@gmail.com>
2023-08-03 13:57:53 +0300

    portfolio

---
+++ shivaniwebsite/src/App.js
@@ -0,0 +1,20 @@
+import NavBar from './components/NavBar.js'
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      
+    App
+          
+          <NavBar />
+
+ 
+     
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ shivaniwebsite/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function NavBar() {
+  return (
+    <header class= "navbar">
+
+
+        
+    </header>
+  )
+}
+
+
+export default NavBar
